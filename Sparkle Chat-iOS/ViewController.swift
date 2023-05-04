23c47405f16ba6e529091b68183e6ce4227a800c
Timmy Cole <greyhorror30@gmail.com>
2023-05-04 22:13:41 +0630

    Initial Commit

--- Sparkle Chat-iOS/ViewController.swift
+++ Sparkle Chat-iOS/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var facebookLoginButton: UIButton!
+    @IBOutlet var gmailLoginButton: UIButton!
+    @IBOutlet var registerButton: UIButton!
+    
+    func roundedButtons () {
+        facebookLoginButton.layer.cornerRadius = facebookLoginButton.bounds.size.width / 10
+        gmailLoginButton.layer.cornerRadius = gmailLoginButton.bounds.size.width / 10
+        registerButton.layer.cornerRadius = registerButton.bounds.size.width / 10
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        roundedButtons()
+        
     }
 
 
